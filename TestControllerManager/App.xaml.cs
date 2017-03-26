9dd047b97c15cd56f05e074d7bdab2419d25a198
juba <balazs.juhasz@siemens.com>
2017-03-26 19:58:50 +0200

    origins of test mode (simulated TFS connection)

--- TestControllerManager/App.xaml.cs
+++ TestControllerManager/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 using Microsoft.TeamFoundation.Build.Client;
 
@@ -18,14 +19,22 @@ namespace TestControllerManager
         private void OnStartup(object sender, StartupEventArgs e)
         {
             var ioc = new Container();
-            ioc.Register<ITestControllerFactory, TestControllerFactory>(Lifestyle.Singleton);
-            ioc.Register<IBuildServer, BuildServerWrapper>(Lifestyle.Singleton);
-            ioc.Register<IConfiguration, Configuration>(Lifestyle.Singleton);
+
+            if (e.Args.Length > 0 && e.Args[1].Equals("/test", StringComparison.InvariantCulture))
+            {
+                
+            }
+            else
+            {
+                ioc.Register<ITestControllerFactory, TestControllerFactory>(Lifestyle.Singleton);
+                ioc.Register<IBuildServer, BuildServerWrapper>(Lifestyle.Singleton);
+                ioc.Register<IConfiguration, Configuration>(Lifestyle.Singleton);
+                ioc.Register<IMainViewModel, MainViewModel>();
+                ioc.Register<MainWindow, MainWindow>();
+                //ioc.Register(() => new Uri("https://tfs.healthcare.siemens.com:8090/tfs/ikm.tpc.projects"), Lifestyle.Singleton);
+            }
             ioc.Register(() => Dispatcher, Lifestyle.Singleton);
             ioc.Register<IDispatcherService, DispatcherService>();
-            ioc.Register<IMainViewModel, MainViewModel>();
-            ioc.Register<MainWindow, MainWindow>();
-            //ioc.Register(() => new Uri("https://tfs.healthcare.siemens.com:8090/tfs/ikm.tpc.projects"), Lifestyle.Singleton);
 
             MainWindow = ioc.GetInstance<MainWindow>();
 
